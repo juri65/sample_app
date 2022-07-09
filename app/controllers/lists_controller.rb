7d0ff20c0e3f4d4b5862fc87ccdf516784f2c052
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-07-09 05:17:34 +0000

    [Add]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,30 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
-
+  
+  def create
+    #1.$2.データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #3.データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #4.トップ画面へリダイレクト
+    redirect_to'/top'
+  end 
+  
   def index
-  end
-
+  end 
+  
   def show
-  end
-
+  end 
+  
   def edit
-  end
+  end 
+  
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end 
 end
