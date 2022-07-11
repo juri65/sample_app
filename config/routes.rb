fb090c893036a915ac861b1f2ac4958aabff5c73
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-07-11 00:57:11 +0000

    [Add]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   post 'lists'=> 'lists#create'
   get 'lists'=>'lists#index'
   #.../lists/1 や.../lists/3　に該当する
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show',as:'list'
+  get 'lists/:id/edit' => 'lists#edit',as:'edit_list'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
